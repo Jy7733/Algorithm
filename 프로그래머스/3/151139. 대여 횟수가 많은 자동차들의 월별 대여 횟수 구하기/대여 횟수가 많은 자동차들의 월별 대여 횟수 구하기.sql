683913beca83b558d34c2e86ab71f628230fdb74
Jaeyoung You <jaeyoung0703@naver.com>
2025-10-16 16:54:22 +0900

    [level 3] Title: 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
+++ 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
@@ -1,22 +1,24 @@
--- 대여시작일 기준 (2022-08 ~ 2022-10까지 ) 총 대여 횟수가 5회 이상인 자동차 중 
-        -- 해당 기간 동안의 월별, 자동차id별 총 대여 횟수 
-        -- 특정 월의 총 대여횟수가 0이면 제외 
--- 월 오름차순, id 내림차순  
-
-select
+-- 코드를 입력하세요
+-- 2022년 8월 ~ 2022년 10월 - 총 대여 횟수가 5회 이상인 자동차 중 
+    -- 월별 + 자동차id별 총 대여 횟수 
+-- order by 월, 자동차id desc
+SELECT
 month(start_date) as month, 
 car_id, 
-count(*) as records
-    from 
-    CAR_RENTAL_COMPANY_RENTAL_HISTORY
-        where car_id in 
-        (select car_id 
-        from CAR_RENTAL_COMPANY_RENTAL_HISTORY
-        where year(start_date) = 2022
-        and month(start_date) between 8 and 10 
-         group by car_id
-        having count(*) >= 5
-        )
-        and year(start_date) = 2022 and month(start_date) between 8 and 10 
-group by month, car_id
+count(car_id) as records 
+from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+
+
+where car_id in 
+-- 대여횟수가 5회 이상
+(
+    select car_id
+    from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+    where start_date between '2022-08-01' and '2022-10-31'
+    group by car_id
+    having(count(car_id) >= 5)
+)
+and 
+start_date between '2022-08-01' and '2022-10-31 '
+group by month(start_date), car_id
 order by month, car_id desc;
