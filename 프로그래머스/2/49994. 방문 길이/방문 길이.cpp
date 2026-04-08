3ad260e86bf2d1f4dce879986d27f8686c9960a0
Jaeyoung You <jaeyoung0703@naver.com>
2026-04-08 17:25:49 +0900

    [level 2] Title: 방문 길이, Time: 0.04 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
@@ -1,62 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool visited[11][11][4]; 
+bool visited[4][501][501];
 
-/*
-0: U
-1: D
-2: L
-3: R
-*/
+int dx[] = {-1,1,0,0};
+int dy[] = {0,0,-1,1};
 
 int solution(string dirs) {
     int answer = 0;
     
-    //시작점 설정
-    int sx = 5;
-    int sy = 5; 
-    
-    //이동 
-    for(auto dir : dirs){
-        int nx=0, ny=0; //이동할 위치 
-        int ndir =0; //이동한 방향 
-        int cmpdir = 0; //비교할 방향 
-        
-        if(dir == 'U'){
-            nx = sx-1;
-            ny = sy; 
-            ndir = 0; 
-            cmpdir = 1;
+    int cx=5, cy=5;
+    int nx=0, ny=0, nd=0, pd=0;
+    for(auto d:dirs){
+        if(d == 'U'){
+            nx = cx-1;
+            ny = cy;
+            nd=0;
+            pd=1;
         }
-        else if(dir == 'D'){
-            nx = sx+1;
-            ny = sy;
-            ndir = 1; 
-            cmpdir = 0;
+        else if(d=='D'){
+            nx = cx+1;
+            ny = cy;
+            nd=1;
+            pd=0;
         }
-        else if(dir == 'L'){
-            nx = sx;
-            ny = sy-1;
-            ndir = 2;
-            cmpdir = 3;
+        else if(d=='L'){
+            nx = cx;
+            ny = cy-1;
+            nd=2;
+            pd=3;
         }
-        else if(dir == 'R'){
-            nx = sx;
-            ny = sy+1;
-            ndir = 3;
-            cmpdir = 2;
+        else if(d=='R'){
+            nx = cx;
+            ny = cy+1;
+            nd=3;
+            pd=2;
         }
         
-        //범위 체크 
-        if(nx < 0 || nx > 10 || ny < 0 || ny > 10) continue; //범위를 넘어갈 경우 무시
+        if(nx<0 || nx > 10 || ny<0 || ny>10) continue;
         
-        if(!visited[nx][ny][ndir] && !visited[sx][sy][cmpdir]){
-            visited[nx][ny][ndir] = true;
+        if(!visited[nd][nx][ny] && !visited[pd][cx][cy]){
             answer++;
         }
-        sx = nx;
-        sy = ny;
+        visited[nd][nx][ny] = true;
+        
+        cx=nx;
+        cy=ny;
     }
+    
     return answer;
 }
