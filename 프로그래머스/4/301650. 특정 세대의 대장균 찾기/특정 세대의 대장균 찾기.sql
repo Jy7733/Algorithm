64ce8761e3ede1c00d5a8e1df9e9e7cb59ea7376
Jaeyoung You <jaeyoung0703@naver.com>
2026-04-10 12:15:39 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,12 +1,51 @@
 -- 코드를 작성해주세요
-with 
-gen1 as (select id as id from ecoli_data where parent_id is null), 
-gen2 as (select e.id from ecoli_data e join gen1 g on g.id = e.parent_id )
 
+with recursive ecoli_tree as (
+    select id, parent_id, 1 as gen from ecoli_data where parent_id is null
+    union all 
+    select e.id, e.parent_id, t.gen+1 
+    from ecoli_tree t 
+    join ecoli_data e on t.id = e.parent_id
+)
+
+select id
+from ecoli_tree
+where gen = 3
+order by id
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+with recursive gen as 
+(
+    select 
+    id,
+    parent_id,
+    1 as generation 
+    from ecoli_data where parent_id is null 
+    
+    union all 
+    
+    select e.id, e.parent_id, g.generation+1 
+    from ecoli_data e 
+    join gen g on e.parent_id = g.id 
+    
+)
 
 select 
-e.id
-from ecoli_data e
-join gen2 g on g.id=e.parent_id
-order by e.id
-
+count(*) as count,
+generation
+from gen
+where id not in (select distinct parent_id from ecoli_data where parent_id is not null)
+group by generation
+*/
