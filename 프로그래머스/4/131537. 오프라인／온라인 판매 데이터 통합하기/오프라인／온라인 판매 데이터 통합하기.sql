71f280e70dd7cdbc5e4ae9a4f29a2dad520e8be7
Jaeyoung You <jaeyoung0703@naver.com>
2026-04-10 12:25:47 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,20 +1,17 @@
 -- 코드를 입력하세요
-
-select 
+select
     date_format(sales_date,'%Y-%m-%d') as sales_date,
     product_id,
     user_id,
     sales_amount
-    from online_sale
-    where month(sales_date) = 3
-union
-(
-    select
+from online_sale
+where year(sales_date) = 2022 and month(sales_date) = 3
+union all 
+select 
     date_format(sales_date,'%Y-%m-%d') as sales_date,
     product_id,
     null as user_id,
     sales_amount
-    from offline_sale
-    where month(sales_date) = 3
-)
+from offline_sale
+where year(sales_date) = 2022 and month(sales_date) = 3
 order by sales_date, product_id, user_id
