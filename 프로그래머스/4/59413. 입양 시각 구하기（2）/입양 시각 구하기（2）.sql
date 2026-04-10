154cc3e30cfedf9fbdda02b97ed466a3a808e035
Jaeyoung You <jaeyoung0703@naver.com>
2026-04-10 12:08:48 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,4 +1,33 @@
 -- 코드를 입력하세요
+with recursive t as (
+    select 0 as hour
+    union all
+    select t.hour+1 from t where hour<23
+)
+
+select
+t.hour,
+count(hour(o.datetime)) as count
+from animal_outs o
+right join t on t.hour = hour(o.datetime)
+group by hour
+order by hour
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 with recursive hours as (
     select 0 as hour 
     union all 
@@ -13,6 +42,4 @@ from animal_outs o
 right join 
 hours h on h.hour = hour(o.datetime)
 group by h.hour
-order by h.hour
-
-
+order by h.hour*/
